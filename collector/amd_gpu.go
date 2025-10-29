cbbe5d07fa3828233864beead063d33303080ea6
Aiden Mitchell <aiden.mitchell@amd.com>
2025-10-29 17:03:42 -0600

    Missing field for gpu metrics

--- collector/amd_gpu.go
+++ collector/amd_gpu.go
@@ -50,43 +50,43 @@ func NewAMDGPUCollector() (*AMDGPUCollector, error) {
 		gpuBusyPercent: prometheus.NewDesc(
 			"syscraper_gpu_busy_percent",
 			"Percentage GPU is busy.",
-			[]string{"card", "model"},
+			[]string{"card", "id"},
 			nil,
 		),
 		gpuGTTSize: prometheus.NewDesc(
 			"syscraper_gpu_gtt_size",
 			"Size of GTT block in bytes.",
-			[]string{"card", "model"},
+			[]string{"card", "id"},
 			nil,
 		),
 		gpuGTTUsed: prometheus.NewDesc(
 			"syscraper_gpu_gtt_used",
 			"Used bytes of GTT block.",
-			[]string{"card", "model"},
+			[]string{"card", "id"},
 			nil,
 		),
 		gpuVisibleVRAMSize: prometheus.NewDesc(
 			"syscraper_gpu_visible_vram_size",
 			"Size of visible VRAM in bytes.",
-			[]string{"card", "model"},
+			[]string{"card", "id"},
 			nil,
 		),
 		gpuVisibleVRAMUsed: prometheus.NewDesc(
 			"syscraper_gpu_visible_vram_used",
 			"Used bytes of visible VRAM.",
-			[]string{"card", "model"},
+			[]string{"card", "id"},
 			nil,
 		),
 		gpuMemoryVRAMSize: prometheus.NewDesc(
 			"syscraper_gpu_vram_size",
 			"Size of VRAM in bytes.",
-			[]string{"card", "model"},
+			[]string{"card", "id"},
 			nil,
 		),
 		gpuMemoryVRAMUsed: prometheus.NewDesc(
 			"syscraper_gpu_vram_used",
 			"Used bytes of VRAM.",
-			[]string{"card", "model"},
+			[]string{"card", "id"},
 			nil,
 		),
 	}, nil
@@ -107,43 +107,43 @@ func (gc *AMDGPUCollector) Collect(ch chan<- prometheus.Metric) {
 			gc.gpuBusyPercent,
 			prometheus.GaugeValue,
 			float64(card.GPUBusyPercent),
-			card.Name,
+			card.Name, card.UniqueID,
 		)
 		ch <- prometheus.MustNewConstMetric(
 			gc.gpuGTTSize,
 			prometheus.GaugeValue,
 			float64(card.MemoryGTTSize),
-			card.Name,
+			card.Name, card.UniqueID,
 		)
 		ch <- prometheus.MustNewConstMetric(
 			gc.gpuGTTUsed,
 			prometheus.GaugeValue,
 			float64(card.MemoryGTTUsed),
-			card.Name,
+			card.Name, card.UniqueID,
 		)
 		ch <- prometheus.MustNewConstMetric(
 			gc.gpuVisibleVRAMSize,
 			prometheus.GaugeValue,
 			float64(card.MemoryVisibleVRAMSize),
-			card.Name,
+			card.Name, card.UniqueID,
 		)
 		ch <- prometheus.MustNewConstMetric(
 			gc.gpuVisibleVRAMUsed,
 			prometheus.GaugeValue,
 			float64(card.MemoryVisibleVRAMUsed),
-			card.Name,
+			card.Name, card.UniqueID,
 		)
 		ch <- prometheus.MustNewConstMetric(
 			gc.gpuMemoryVRAMSize,
 			prometheus.GaugeValue,
 			float64(card.MemoryVRAMSize),
-			card.Name,
+			card.Name, card.UniqueID,
 		)
 		ch <- prometheus.MustNewConstMetric(
 			gc.gpuMemoryVRAMUsed,
 			prometheus.GaugeValue,
 			float64(card.MemoryVRAMUsed),
-			card.Name,
+			card.Name, card.UniqueID,
 		)
 	}
 
